50233453039fd39d38564cb111b5a509aa07f68a
Vladislav <93011297+VladislavApelhants@users.noreply.github.com>
2025-02-05 16:32:45 +0200

    fix-logo

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -1,27 +1,27 @@
-import Image from 'next/image';
-import { SocialList } from '@/components/shared/SocialList/SocialList';
-import { Navigation } from '@/components/layout/Header/Navigation/Navigation';
-import { GoogleMapLink } from '@/components/layout/Header/GoogleMapLink/GoogleMapLink';
 import { Calendar } from '@/components/layout/Header/Calendar/Calendar';
-import { Phone } from '@/components/layout/Header/Phone/Phone';
+import { GoogleMapLink } from '@/components/layout/Header/GoogleMapLink/GoogleMapLink';
 import { MobileMenu } from '@/components/layout/Header/MobileMenu/MobileMenu';
-import { Typography } from '@/components/ui/Typography/Typography';
-import Link from 'next/link';
+import { Navigation } from '@/components/layout/Header/Navigation/Navigation';
+import { Phone } from '@/components/layout/Header/Phone/Phone';
 import { GetCallModal } from '@/components/shared/GetCallModal/GetCallModal';
+import { SocialList } from '@/components/shared/SocialList/SocialList';
+import Image from 'next/image';
+import Link from 'next/link';
 
 export const Header = () => {
   return (
     <header>
-      <div className="container flex items-center justify-between py-8">
+      <div className="container flex items-center justify-between py-6 md:py-8">
         <Link href={'/'}>
           <Image src={'/img/logo.png'} alt={'Logo'} width={57} height={51} />
         </Link>
-        <Typography
-          className={'mb-0 text-4xl italic text-blue md:hidden'}
-          variant={'h2'}
-        >
-          Biodent
-        </Typography>
+        <Image
+          src={'/img/logo.svg'}
+          alt={'Logo'}
+          width={130}
+          height={51}
+          className="md:hidden"
+        />
         <SocialList className={'hidden md:flex md:gap-4'} />
         <Navigation className={'hidden md:flex'} />
         <div className={'hidden items-center justify-center gap-5 xl:flex'}>
@@ -29,7 +29,7 @@ export const Header = () => {
           <Calendar />
         </div>
         <Phone className={'hidden md:flex md:gap-2'} />
-        <GetCallModal text={'Замовити дзвінок'} className="hidden"/>
+        <GetCallModal text={'Замовити дзвінок'} className="hidden" />
         <MobileMenu />
       </div>
     </header>
